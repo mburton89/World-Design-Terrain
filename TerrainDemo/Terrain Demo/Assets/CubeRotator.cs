9f90c59c12f21622c772f0d7bb1804703fd9a45f
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2020-08-29 16:17:15 -0500

    Initial Commit

---
+++ TerrainDemo/Terrain Demo/Assets/CubeRotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotator : MonoBehaviour
+{
+    public Vector3 direction;
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(direction * speed);
+    }
+}
