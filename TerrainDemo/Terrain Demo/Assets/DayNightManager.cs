e669bacf44e5754f05083ad90227443e149d0be6
Matt Burton <matthewwburton@gmail.com>
2020-10-19 18:23:10 -0500

    Added Post Processing and Dungeon Stuff

---
+++ TerrainDemo/Terrain Demo/Assets/DayNightManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class DayNightManager : MonoBehaviour
+{
+    public Gradient AmbientColor;
+    [SerializeField] private Light DirectionalLight;
+    [SerializeField, Range(0, 24)] private float TimeOfDay;
+    [SerializeField] public float slownessMultiplier;
+
+    private void Update()
+    {
+        if (Application.isPlaying)
+        {
+            //(Replace with a reference to the game time)
+            TimeOfDay += Time.deltaTime * slownessMultiplier;
+            TimeOfDay %= 24; //Modulus to ensure always between 0-24
+            UpdateLighting(TimeOfDay / 24f);
+        }
+        else
+        {
+            UpdateLighting(TimeOfDay / 24f);
+        }
+    }
+
+    private void UpdateLighting(float timePercent)
+    {
+        RenderSettings.ambientLight = AmbientColor.Evaluate(timePercent);
+
+        if (DirectionalLight != null)
+        {
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (DirectionalLight != null)
+            return;
+    }
+}
