918202634fccebf694cd9d2a1c43db291cc1776e
Matt Burton <matthewwburton@gmail.com>
2020-11-17 16:10:15 -0600

    Added home menu and other components

---
+++ TerrainDemo/Terrain Demo/Assets/MainMenuComponents/Rotator2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator2 : MonoBehaviour
+{
+    public Vector3 direction;
+    public float speed;
+
+    void Update()
+    {
+        transform.Rotate(direction * speed);
+    }
+}
