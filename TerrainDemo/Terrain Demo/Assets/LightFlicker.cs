97c93fa925e57d5387ecf43e9c6e24fa7e9d20f4
Matt Burton <matthewwburton@gmail.com>
2022-04-18 12:26:41 -0500

    All the things including rain

---
+++ TerrainDemo/Terrain Demo/Assets/LightFlicker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    private Light _light;
+    private float _secondsOn;
+    private float _secondsOff;
+
+    private void Awake()
+    {
+        _light = GetComponent<Light>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Flicker());
+    }
+
+    private IEnumerator Flicker()
+    {
+        _secondsOff = Random.Range(0.05f, 0.4f);
+        _secondsOn = Random.Range(0.025f, 0.2f);
+
+        _light.enabled = true;
+        yield return new WaitForSeconds(_secondsOn);
+
+        _light.enabled = false;
+        yield return new WaitForSeconds(_secondsOff);
+
+        StartCoroutine(Flicker());
+    }
+}
---
+++ TerrainDemo/Terrain Demo/Assets/SwitchingScenes/Code/SceneSwitchManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitchManager : MonoBehaviour
+{
+    public static SceneSwitchManager Instance;
+    public List<Transform> spawnPoints;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void MoveToSpawnPoint(Transform transform, int spawnPointIndex)
+    {
+        transform.position = spawnPoints[spawnPointIndex].position;
+    }
+}
